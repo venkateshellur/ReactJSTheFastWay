4c9c412fa0d26894d18a133ff51c55c67786823a
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-05 13:34:31 -0500

    14. Created the Header Component and included in the App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css'; // Assuming you have a styles.css file for additional styles
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Welcome to Movideux</h1>
-      </header>
+      <div className="container">
+      <Header />
+     </div>
       <footer className="footer">
         <p>© 2023 Venky's React App Learning</p>
       </footer>
