5c224598b7ac1fa4afef8b2813943e7c10f0c217
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-06 00:07:18 -0500

    35. Implement ynamic styles for rating

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,6 +6,12 @@ export default function MovieCard({ movie }) {
     const handleError = (event) => {
         event.target.src = "images/default.jpg"; // Fallback image
     }
+    
+    const getRatingClass = (rating) => {
+        if (rating >= 8) return "rating-good";
+        if (rating >= 5) return "rating-ok";
+        return "rating-bad";}
+        console.log("MovieCard rendered for:", movie.title);
 
   return (
     <div className="movie-card" key={movie.id}>
@@ -13,7 +19,7 @@ export default function MovieCard({ movie }) {
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
-        <p className="movie-card-rating">{movie.rating}</p>
+        <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>{movie.rating}</p>
       </div>
     </div>
   );
