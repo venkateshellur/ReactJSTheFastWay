4427b7724982a5de1bd04b1a83b064cbbb69c982
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-05 23:57:44 -0500

    32. Handling error

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,13 +2,18 @@ import React from "react";
 import "../styles.css";
 
 export default function MovieCard({ movie }) {
+
+    const handleError = (event) => {
+        event.target.src = "images/default.jpg"; // Fallback image
+    }
+
   return (
     <div className="movie-card" key={movie.id}>
-      <img src={`images/${movie.image}`} alt={movie.title} />
+      <img src={`images/${movie.image}`} alt={movie.title} onError={handleError} />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
-        <p className="movie-card-rating">Rating: {movie.rating}</p>
+        <p className="movie-card-rating">{movie.rating}</p>
       </div>
     </div>
   );
