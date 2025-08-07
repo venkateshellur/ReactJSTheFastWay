0a2c9732fdd1fdd7db247c52d51976b4eafb000a
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-06 22:40:30 -0500

    36. Filter movies via search

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,14 +5,36 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("movies.json")
-    .then(response => response.json())
-    .then(data => setMovies(data))
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
   }, []);
 
-  return <div className="movies-grid">
-    {movies.map(movie => (<MovieCard movie={movie} />))};
-  </div>;
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovis = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search Movies..."
+        className="search-input"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovis.map((filteredMovis) => (
+          <MovieCard movie={filteredMovis} />
+        ))}
+      </div>
+    </div>
+  );
 }
