10f7b28a765a74f3d9f7fd5725d968ec38cebcf0
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-07 00:12:47 -0500

    44. Lifting the State one level Up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import './App.css';
-import './styles.css'; // Assuming you have a styles.css file for additional styles
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "./styles.css"; // Assuming you have a styles.css file for additional styles
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist"; // Importing Watchlist component
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
-      <Header />
-      <MoviesGrid />
-     </div>
-     <Footer />
+        <Header />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+                <Link to="/Watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid movies={movies} />}></Route>
+            <Route path="/Watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,19 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import "../styles.css";
 import MovieCard from "./MovieCard";
 
-export default function MoviesGrid() {
-  const [movies, setMovies] = useState([]);
+export default function MoviesGrid({ movies }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [genere, setGenere] = useState("All Genere");
   const [rating, setRating] = useState("All");
-
-  useEffect(() => {
-    fetch("movies.json")
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
-  }, []);
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
