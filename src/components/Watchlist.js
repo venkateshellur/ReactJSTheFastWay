b065adb947cc638305577cf0315c67454a28de04
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-07 11:45:37 -0500

    47. Made the Watchlist functional.

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "../styles.css";
+import MovieCard from "./MovieCard";
 
 export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
   return (
     <div>
       <h1 className="title">Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          console.log(movie);
+          return (
+            <MovieCard
+              movie={movie}
+              key={id}
+              toggleWatchlist={toggleWatchlist}
+              isWatchlisted={true}
+            ></MovieCard>
+          );
+        })}
+      </div>
     </div>
   );
 }
