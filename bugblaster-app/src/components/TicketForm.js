18efeae82308340b1a305e335d7b766653305287
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-08 14:43:12 -0500

    62. Create the TicketForm UX and include it in the app.js

---
+++ bugblaster-app/src/components/TicketForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+export default function TicketForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("1");
+
+  const priorityLables = { 1: "Low", 2: "Medium", 3: "High" };
+
+  const clearForm = () => {
+    setTitle("");
+    setDescription("");
+    setPriority("1");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearForm();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="ticket-form">
+      <div>
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          className="form-input"
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label>description</label>
+        <textarea
+          value={description}
+          className="form-input"
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </div>
+      <fieldset className="priority-fieldset">
+        <legend>priority</legend>
+        {Object.entries(priorityLables).map(([value, label]) => (
+          <label key={value} className="priority-label">
+            <input
+              type="radio"
+              value={value}
+              checked={priority === value}
+              className="priority-input"
+              onChange={(e) => setPriority(e.target.value)}
+            ></input>
+            {label}
+          </label>
+        ))}
+      </fieldset>
+      <button type="submit" className="button">
+        Submit
+      </button>
+    </form>
+  );
+}
