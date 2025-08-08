fe4ec2815e79ebbd99e30b68028ac636be240ef7
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-08 15:50:18 -0500

    69. Display the list of tickets created in the same page after submitting each ticket.

--- bugblaster-app/src/App.js
+++ bugblaster-app/src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
 import ticketReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
 
 function App() {
   const initialState = { tickets: [] };
@@ -15,6 +16,17 @@ function App() {
       <div className="container">
         <h1>Bug Blaster</h1>
         <TicketForm dispatch={dispatch}></TicketForm>
+        {state.tickets.length > 0 ? (
+          <div className="results">
+            <h2>All Tickets</h2>
+            <TicketList
+              tickets={state.tickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        ) : (
+          <h2> There are no Tickets Created</h2>
+        )}
       </div>
     </div>
   );
---
+++ bugblaster-app/src/components/TicketItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function TicketItem({ ticket, dispatch }) {
+  const { id, title, description, priority } = ticket;
+  const priorityClass = {
+    1: "priority-low",
+    2: "priority-medium",
+    3: "priority-high",
+  };
+
+  return (
+    <div className="ticket-item">
+      <div className={`priority-dot ${priorityClass[priority]}`}></div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
