175b1c125223f0c74f65b17460b7335c9f472c4a
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-07 22:54:15 -0500

    59. Initial Setup of BUBLASTER App

---
+++ bugblaster-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bug Blaster</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
