c8e5027586cf459defbc9dedad711ed313f804c9
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-10 22:03:24 -0500

    81. combine context with state initial checkin

--- blogapp/src/components/Comment.js
+++ blogapp/src/components/Comment.js
@@ -5,6 +5,7 @@ export default function Comment() {
   const { username, isAdmin } = useContext(UserInfoContext);
   return (
     <div>
+      {isAdmin && <button>Delete</button>}
       <p>Logged in as {username}</p>
       {isAdmin && <button>Edit Comment</button>}
     </div>
--- blogapp/src/components/Post.js
+++ blogapp/src/components/Post.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Comment from "./Comment";
+import UserInfoContext from "../context/UserInfoontext";
 
 export default function Post() {
+  const { isAdmin } = useContext(UserInfoContext);
+
   return (
     <div>
+      {isAdmin && <button>Delete</button>}
       <h1>Example Post Title</h1>
       <p>This isan example post content.</p>
 
