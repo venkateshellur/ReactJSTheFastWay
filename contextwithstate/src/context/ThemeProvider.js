a2526a1a0d543bb9f3ea4b7483a09bed2fe325ec
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-12 11:20:53 -0500

    83. contextAPI training material cheatsheet included.

--- contextwithstate/src/context/ThemeProvider.js
+++ contextwithstate/src/context/ThemeProvider.js
@@ -1,16 +1,18 @@
-import ThemeContext from "./context/ThemeContext";
-import { Children, React, useState } from "react";
+import ThemeContext from "../context/ThemeContext";
+import { React, useState } from "react";
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
+    console.log("Current Theme is : " + theme);
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return (
-    <ThemeContext.Provider value={(theme, setTheme)}>
-      {{ children }}
+    // Even the simple braces matter a lot in the below code the inner flower braces were replaced with round brackets and everything stopped working with no errors displayed.
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
     </ThemeContext.Provider>
   );
 };
