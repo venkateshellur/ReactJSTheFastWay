d72d3efa4505cd5fb3a62d313d9b9141bdbd817d
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-13 23:15:36 -0500

    107, on accessing the /books route, navigate  the user to the required route.

--- booklibrary/src/App.js
+++ booklibrary/src/App.js
@@ -2,13 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BooksProvider } from "./BooksContext";
 import Books from "./components/Books";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <BooksProvider>
-      <div>
-        <Books></Books>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books></Books>}></Route>
+          <Route path="/books" element={<Navigate to="/" />}></Route>
+        </Routes>
+      </Router>
     </BooksProvider>
   );
 }
