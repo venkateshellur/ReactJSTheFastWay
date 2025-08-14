3c4781a3e45e752ec7e236545e4093c69f34ea51
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-13 23:56:50 -0500

    110. Simple implementation of 404 NotFound using route.

--- booklibrary/src/App.js
+++ booklibrary/src/App.js
@@ -10,6 +10,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import BookDetails from "./components/BookDetails";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
             path="/books/:bookId"
             element={<BookDetails></BookDetails>}
           ></Route>
+          <Route path="*" element={<NotFound></NotFound>}></Route>
         </Routes>
       </Router>
     </BooksProvider>
---
+++ booklibrary/src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+  return <h2>404 not found :-( </h2>;
+}
