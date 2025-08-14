401250da766d70626731321508170a912aeb7427
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-13 23:48:46 -0500

    109. Display the books based on the id parameter and display a book not found message if there is no book for the provided id.

--- booklibrary/src/components/BookDetails.js
+++ booklibrary/src/components/BookDetails.js
@@ -1,11 +1,22 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { BooksContext } from "../BooksContext";
 import { useParams } from "react-router-dom";
 
 export default function BookDetails() {
-  const bookID = useParams();
-  useEffect(() => {
-    console.log(bookID);
-  });
-  return <></>;
+  const { bookId } = useParams();
+  const books = useContext(BooksContext);
+  const book = books.find((b) => b.id === parseInt(bookId));
+
+  if (book == null) {
+    return <div> Book not found for the provided id {bookId}</div>;
+  } else
+    return (
+      <>
+        <h1>Book found for the provided Book ID : {bookId}</h1>
+        <h2>
+          the book named '{book.title}' by the Author '{book.author}'
+        </h2>
+        <p>{book.description}</p>
+      </>
+    );
 }
