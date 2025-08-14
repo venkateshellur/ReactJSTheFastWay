6e246f0a006429342eae50105c1bdaeb120fc961
venkateshellur <venkatesh.ellur@gmail.com>
2025-08-14 00:10:38 -0500

    111. Read the Query string and log to console to cross verify if able to read the query string.

--- booklibrary/src/components/Books.js
+++ booklibrary/src/components/Books.js
@@ -1,8 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { BooksContext } from "../BooksContext";
 
 export default function Books() {
+  const query = new URLSearchParams(useLocation().search);
+  const search = query.get("search") || "";
   const books = useContext(BooksContext);
+
+  //Try using this Query string and check console if the URLSearchParams is logged to console along with the search string 'Venky
+  //http://localhost:3000/?search=Venky
+  useEffect(() => {
+    console.log(query);
+    console.log(search);
+  });
+
   return (
     <div>
       <h1> All Books</h1>
